2b598321eb2d6255893b597d0352796f2efc7ead
Randy Ortiz <Randyortiz28@gmail.com>
2020-09-24 16:14:34 -0400

    fixes

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ init();
 
     function navBar() {
       var x = document.getElementById("navbar");
-      if (x.style.display === "block") {
+      if (x.style.display === "flex") {
           x.style.display = "none";
   } else {
-          x.style.display = "block";
+          x.style.display = "flex";
           }
         }
 
